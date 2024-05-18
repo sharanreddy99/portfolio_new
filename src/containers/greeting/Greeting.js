0612dfefcddfaac0a1544ae1cb1f6a458c780426
Sharan Reddy <sharansaikonda99@gmail.com>
2024-05-18 10:40:09 -0500

    fix infinite loop on button click

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -7,55 +7,61 @@ import { Fade } from "react-reveal";
 import FeelingProud from "./FeelingProud";
 import ResumeModal from "../../components/ResumeModal/ResumeModal";
 
-export default function Greeting(props) {
-  const theme = props.theme;
-
+const Greeting = ({ theme }) => {
   const [showModal, setShowModal] = useState(false);
 
   return (
-    <Fade bottom duration={2000} distance="40px">
-      <div className="greet-main" id="greeting">
-        <div className="greeting-main">
-          <div className="greeting-text-div">
-            <div>
-              <h1 className="greeting-text" style={{ color: theme.text }}>
-                {greeting.title}
-              </h1>
-              {greeting.nickname && (
-                <h2 className="greeting-nickname" style={{ color: theme.text }}>
-                  ( {greeting.nickname} )
-                </h2>
-              )}
-              <p
-                className="greeting-text-p subTitle"
-                style={{ color: theme.secondaryText }}
-              >
-                {greeting.subTitle}
-              </p>
-              <SocialMedia theme={theme} />
-              <div className="portfolio-repo-btn-div">
-                <Button
-                  text="Resume"
-                  newTab={true}
-                  onClick={() => {
-                    setShowModal(true);
-                  }}
-                  theme={theme}
-                  className="portfolio-repo-btn"
-                />
+    <>
+      <ResumeModal
+        show={showModal}
+        setShow={setShowModal}
+        resumeLink={greeting.resumeLink}
+      />
+
+      <Fade bottom duration={2000} distance="40px">
+        <div className="greet-main" id="greeting">
+          <div className="greeting-main">
+            <div className="greeting-text-div">
+              <div>
+                <h1 className="greeting-text" style={{ color: theme.text }}>
+                  {greeting.title}
+                </h1>
+                {greeting.nickname && (
+                  <h2
+                    className="greeting-nickname"
+                    style={{ color: theme.text }}
+                  >
+                    ( {greeting.nickname} )
+                  </h2>
+                )}
+                <p
+                  className="greeting-text-p subTitle"
+                  style={{ color: theme.secondaryText }}
+                >
+                  {greeting.subTitle}
+                </p>
+                <SocialMedia theme={theme} />
+                <div className="portfolio-repo-btn-div">
+                  <Button
+                    text="Resume"
+                    newTab={true}
+                    onClick={() => {
+                      setShowModal(true);
+                    }}
+                    theme={theme}
+                    className="portfolio-repo-btn"
+                  />
+                </div>
               </div>
-              <ResumeModal
-                show={showModal}
-                setShow={setShowModal}
-                resumeLink={greeting.resumeLink}
-              />
+            </div>
+            <div className="greeting-image-div">
+              <FeelingProud theme={theme} />
             </div>
           </div>
-          <div className="greeting-image-div">
-            <FeelingProud theme={theme} />
-          </div>
         </div>
-      </div>
-    </Fade>
+      </Fade>
+    </>
   );
-}
+};
+
+export default Greeting;
